d1f4c88ccd3b04ce370a9d489395be814a0f8a7c
doseeing <doseeing@gmail.com>
2021-04-16 16:13:12 +0800

    chore: change base url

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const rootURL = "https://nextjs-zeta-bice.vercel.app";
+  const rootURL = "https://kissapi-qrcode.vercel.app";
   return (
     <div className={styles.container}>
       <Head>
